a59dcb7e64180c323efdcbfad6e285040d75735a
Pablo Guillermo <pablodanielguillermo@hotmail.com>
2024-07-15 17:31:05 -0300

    Commit 15/7

---
+++ 18_I02_Entidades/Caja.cs
@@ -0,0 +1,46 @@
+﻿namespace _18_I02_Entidades
+{
+    public delegate void delegadoClienteAtendido(Caja caja, string str);
+    public class Caja
+    {
+        private static Random random;
+        private Queue<string> clientesALaEspera;
+        private string nombreCaja;
+        private delegadoClienteAtendido DelegadoClienteAtentido;
+
+        public string Nombre { get; }
+        public int CantidadDeClientesALaEspera { get { return clientesALaEspera.Count; } }
+
+        static Caja()
+        {
+            random = new Random();
+        }
+
+        public Caja(string nombreCaja, delegadoClienteAtendido delegadoClienteAtentido)
+        {
+            this.nombreCaja = nombreCaja;
+            this.clientesALaEspera = new Queue<string>();
+            this.DelegadoClienteAtentido = delegadoClienteAtentido;
+        }
+
+        //Tendrá un método AgregarCliente con visibilidad internal que recibirá un cliente y lo agregará a clientesALaEspera.
+        internal void AgregarCliente(string cliente)
+        {
+            clientesALaEspera.Enqueue(cliente);
+        }
+
+        //Tendrá un método IniciarAtencion con visibilidad internal que deberá iniciar la atención de clientes en un sub-proceso paralelo.
+        //Este método no recibirá nada y retornará la instancia de Task que se haya utilizado.
+        internal Task IniciarAtencion()
+        {
+            if (clientesALaEspera.Any<string>())
+            {
+                string cliente = clientesALaEspera.Dequeue();
+                Task task = Task.Run(() => DelegadoClienteAtentido(this, cliente));
+                Thread.Sleep(random.Next(1000, 5000));
+                return task;
+            }
+            return null;
+        }
+    }
+}
---
+++ 18_I02_Entidades/Negocio.cs
@@ -0,0 +1,47 @@
+﻿using NameGenerator.Generators;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_I02_Entidades
+{
+    public class Negocio
+    {
+        private static RealNameGenerator generadorNombres;
+        private List<Caja> cajas;
+        private ConcurrentQueue<string> clientes;
+        private bool flag = true;
+
+        static Negocio()
+        {
+            generadorNombres = new RealNameGenerator();
+        }
+
+        public Negocio(List<Caja> cajas)
+        {
+            this.cajas = cajas;
+            clientes = new ConcurrentQueue<string>();
+        }
+
+        public void ComenzarAtencion()
+        {
+            foreach (Caja caja in cajas)
+            {
+                caja.IniciarAtencion();
+            }
+            while(flag == true)
+            {
+                Task task = Task.Run(() => clientes.Enqueue(generadorNombres.Generate()));
+                Thread.Sleep(1000);
+            }
+            while (flag == true)
+            {
+                Task task = Task.Run(() => clientes.Enqueue(generadorNombres.Generate()));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
